1522915205ca9aedab3360d6a9b5c3db56ad900b
mrsaicharan1 <saicharan.reddy1@gmail.com>
2018-10-02 20:00:27 +0530

    Dataset prep,Data preprocessing and Linear Reg.

--- data.py
+++ data.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import quandl
+import quandl,math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
-print(df)
 
 df = df[['Adj. High','Adj. Low','Adj. Open','Adj. Close','Adj. Volume']]
 
@@ -12,4 +14,29 @@ df['PCT_change'] = (df['Adj. High'] - df['Adj. Low'])/df['Adj. Low'] * 100.00
 
 df = df[['Adj. Close','PCT_change','HL_PCT','Adj. Volume']]
 
-print(df.head())
+forecast_col = 'Adj. Close'
+
+df.fillna(-99999,inplace=True)
+
+# print(df.tail())
+
+days = int(math.ceil(0.1*len(df)))
+
+df['label'] = df[forecast_col].shift(-days)
+
+df.dropna(inplace=True)
+
+print(df)
+
+X = np.array(df.drop(['label'],1))
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+
+clf.fit(X_train,y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
